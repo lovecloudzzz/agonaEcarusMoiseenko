501e050b7450a318dcb3d8492b31f2a7504de195
lovecloudzzz <lovecloudez@gmail.com>
2023-06-04 19:26:13 +0300

    добавил formik и yup

--- src/components/AuthForm/AuthButton/AuthButton.tsx
+++ src/components/AuthForm/AuthButton/AuthButton.tsx
@@ -1,19 +1,22 @@
-import React, {ReactNode} from 'react';
+import React, { ReactNode, ButtonHTMLAttributes } from 'react';
 import cns from 'classnames/bind';
-import styles from './AuthButton.module.sass'
+import styles from './AuthButton.module.sass';
 
-const cx = cns.bind(styles)
+const cx = cns.bind(styles);
+
 type AuthButtonProps = {
-    color?: "green" | "gray";
+    color?: 'green' | 'gray';
     children: ReactNode;
-}
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export const AuthButton = ({color, children}: AuthButtonProps) => {
+export const AuthButton = ({ color, children, ...props }: AuthButtonProps) => {
     return (
-        <button className={cx(styles.button, {
-            buttonGreen: color === 'green',
-            buttonGray: color === 'gray'
-        })}
+        <button
+            className={cx(styles.button, {
+                buttonGreen: color === 'green',
+                buttonGray: color === 'gray',
+            })}
+            {...props}
         >
             {children}
         </button>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,61 +1,67 @@
-import React, {useState} from 'react';
-import { Link } from "react-router-dom";
-import {AuthForm} from "../AuthForm/AuthForm";
-import styles from './Header.module.sass'
-import currency from "../../assets/Currency.svg";
-import avatar from "../../assets/Avatar.svg";
-import pin from "../../assets/Pin.svg";
-import logo from "../../assets/logo.svg";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthForm } from '../AuthForm/AuthForm';
+import styles from './Header.module.sass';
+import currency from '../../assets/Currency.svg';
+import pin from '../../assets/Pin.svg';
+import logo from '../../assets/logo.svg';
 
 export const Header = () => {
-
-    const [modalAuth, setModalAuth] = useState(false)
+    const [modalAuth, setModalAuth] = useState(false);
 
     return (
         <nav className={styles.Header}>
             <div className={styles.HeaderRow}>
                 <div>
-                    <Link to="/home" className={styles.NoneDecoration}><img src={logo} /></Link>
+                    <Link to="/home" className={styles.NoneDecoration}>
+                        <img src={logo} alt="Logo" />
+                    </Link>
                 </div>
                 <div>
-                    <Link to="/home" className={styles.NoneDecoration}>Главная</Link>
+                    <Link to="/home" className={styles.NoneDecoration}>
+                        Главная
+                    </Link>
                 </div>
                 <div>
-                    <Link to="/points" className={styles.NoneDecoration}>Пункты сбора</Link>
+                    <Link to="/points" className={styles.NoneDecoration}>
+                        Пункты сбора
+                    </Link>
                 </div>
                 <div>
-                    <Link to="/market" className={styles.NoneDecoration}>Экомаркет</Link>
+                    <Link to="/market" className={styles.NoneDecoration}>
+                        Экомаркет
+                    </Link>
                 </div>
                 <div>
-                    <Link to="/about" className={styles.NoneDecoration}>О сервисе</Link>
+                    <Link to="/about" className={styles.NoneDecoration}>
+                        О сервисе
+                    </Link>
                 </div>
             </div>
-            <div className={styles.HeaderRow} onClick={() => setModalAuth(true)}>
+            <div className={styles.HeaderRow}>
                 <div>
                     <a className={styles.HeaderLink}>
-                        <img src={pin}/>
+                        <img src={pin} alt="Pin" />
                         Казань
                     </a>
                 </div>
                 <div>
-                    <Link to="/wallet" className={styles.NoneDecoration}>
-                        <a className={styles.HeaderLink}>
-                            <img src={currency}/>
+                    <a className={styles.HeaderLink}>
+                        <Link to="/wallet" className={styles.NoneDecoration}>
+                            <img src={currency} alt="Currency" />
                             1000
-                        </a>
-                    </Link>
+                        </Link>
+                    </a>
                 </div>
                 <div>
-                    <Link to="/profile" className={styles.NoneDecoration}>
-                        <a className={styles.HeaderLink}>
-                            <img src={avatar}/>
-                            Алексей
-                        </a>
-                    </Link>
+                    <button onClick={() => setModalAuth(true)} className={styles.NoneDecoration}>
+                        Войти
+                    </button>
                 </div>
             </div>
-            <AuthForm modalAuth={modalAuth} closeModal={() => setModalAuth(false)}/>
+            <AuthForm modalAuth={modalAuth} closeModal={() => setModalAuth(false)} />
         </nav>
-
     );
 };
+
+export default Header;
